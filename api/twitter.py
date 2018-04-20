578a8a7c31704b21e1fd58e37721cc056cc0433b
Bruno Santos <bruno.santos1996@gmail.com>
2018-04-20 15:04:10 +0100

    api refactor

---
+++ api/twitter.py
@@ -0,0 +1,97 @@
+import urllib
+from flask import session, request
+from keys import keys_access_token, keys_consumer_key, keys_consumer_secret
+from flask_oauthlib.client import OAuth  # https://github.com/lepture/flask-oauthlib
+
+flask_oauth = None
+access_token = None
+twitter = None
+
+
+# Start Twitter OAuth
+def tw_start_twitter(app):
+	global flask_oauth, access_token, twitter
+	flask_oauth = OAuth(app)
+	access_token = keys_access_token
+	twitter = flask_oauth.remote_app(
+		'twitter',
+		consumer_key=keys_consumer_key,
+		consumer_secret=keys_consumer_secret,
+		base_url='https://api.twitter.com/1.1/',
+		request_token_url='https://api.twitter.com/oauth/request_token',
+		access_token_url='https://api.twitter.com/oauth/access_token',
+		authorize_url='https://api.twitter.com/oauth/authorize'
+	)
+
+	@twitter.tokengetter
+	def get_twitter_token():
+		if 'twitter_oauth' in session:
+			resp = session['twitter_oauth']
+			return resp['oauth_token'], resp['oauth_token_secret']
+
+
+# Get Twitter authorized reply
+def tw_get_authorized_reply():
+	return twitter.authorized_response()
+
+
+# Get Twitter OAuth Login URL
+def tw_get_login_url(callback_url):
+	return twitter.authorize(callback=callback_url or request.referrer or None).location
+
+
+# Make request to Twitter API
+def tw_make_twitter_request(endpoint, method, **params):
+	url = 'https://api.twitter.com/1.1/{0}.json'.format(endpoint)
+	if params:
+		url = '{}?{}'.format(url, urllib.urlencode(params))
+	return twitter.request(url, method=method)
+
+
+# Return tweets that have #buddyrunner
+def tw_filter_runs(data):
+	run_list = []
+	for tweet in data:
+		hashtags = tweet['entities']['hashtags']
+		for hashtag in hashtags:
+			if 'buddyrunner' in hashtag['text']:
+				run_list.append(tweet)
+	return run_list
+
+
+# Return tweets that weren't tweeted by a user
+def tw_filter_friends(data, user_id):
+	run_list = []
+	for tweet in data:
+		if long(tweet['user']['id']) != long(user_id):
+			run_list.append(tweet)
+	return run_list
+
+
+# Return basic run info (id, message, creator)
+def tw_get_run_basic_info(tweet):
+	return {
+		'id': tweet['id'],
+		'text': tweet['text'],
+		'creator': {
+			'id': tweet['user']['id'],
+			'name': tweet['user']['name'],
+			'image': tweet['user']['profile_image_url']
+		}
+	}
+
+
+# Return complete info of a run from id (basic + participants + weather + path)
+def tw_get_run_complete_info(tweet):
+	data = tw_get_run_basic_info(tweet)
+	data['participants'] = []
+	retweets = tw_make_twitter_request('statuses/retweets', 'GET', id=data['id']).data
+	for tweet in retweets:
+		data['participants'].append({
+			'user': {
+				'id': tweet['user']['id'],
+				'name': tweet['user']['name'],
+				'image': tweet['user']['profile_image_url']
+			}
+		})
+	return data
--- app.py
+++ app.py
@@ -1,34 +1,13 @@
 # coding: utf-8
 
-import keys
-import urllib
+from api.twitter import *
 from flask import Flask
 from flask import g, session, request, url_for, json, Response
-from flask_oauthlib.client import OAuth  # https://github.com/lepture/flask-oauthlib
 
 app = Flask(__name__)
 app.debug = True
 app.secret_key = 'development'
-
-oauth = OAuth(app)
-
-access_token = keys.access_token
-twitter = oauth.remote_app(
-	'twitter',
-	consumer_key=keys.consumer_key,
-	consumer_secret=keys.consumer_secret,
-	base_url='https://api.twitter.com/1.1/',
-	request_token_url='https://api.twitter.com/oauth/request_token',
-	access_token_url='https://api.twitter.com/oauth/access_token',
-	authorize_url='https://api.twitter.com/oauth/authorize'
-)
-
-
-@twitter.tokengetter
-def get_twitter_token():
-	if 'twitter_oauth' in session:
-		resp = session['twitter_oauth']
-		return resp['oauth_token'], resp['oauth_token_secret']
+tw_start_twitter(app)
 
 
 @app.before_request
@@ -36,41 +15,6 @@ def before_request():
 	g.user = None
 	if 'twitter_oauth' in session:
 		g.user = session['twitter_oauth']
-
-
-def make_twitter_request(endpoint, method, **params):
-	url = 'https://api.twitter.com/1.1/{0}.json'.format(endpoint)
-	if params:
-		url = '{}?{}'.format(url, urllib.urlencode(params))
-	return twitter.request(url, method=method)
-
-
-def filter_runs(data):
-	run_list = []
-	for tweet in data:
-		hashtags = tweet['entities']['hashtags']
-		for hashtag in hashtags:
-			if 'buddyrunner' in hashtag['text']:
-				run_list.append(tweet)
-	return run_list
-
-
-def filter_friends(data, user_id):
-	run_list = []
-	for tweet in data:
-		if long(tweet['user']['id']) != long(user_id):
-			run_list.append(tweet)
-	return run_list
-
-
-def get_run_basic_info(tweet):
-	return {
-		'id': tweet['id'],
-		'text': tweet['text'],
-		'user_id': tweet['user']['id'],
-		'user_name': tweet['user']['name'],
-		'user_image': tweet['user']['profile_image_url']
-	}
 
 
 @app.route('/')
@@ -81,13 +25,12 @@ def index():
 @app.route('/login')
 def login():
 	callback_url = url_for('oauthorized', next=request.args.get('next'))
-	url = twitter.authorize(callback=callback_url or request.referrer or None)
-	return Response(json.dumps({'url': url.location}), status=200, mimetype='application/json')
+	return Response(json.dumps({'url': tw_get_login_url(callback_url)}), status=200, mimetype='application/json')
 
 
 @app.route('/oauthorized')
 def oauthorized():
-	resp = twitter.authorized_response()
+	resp = tw_get_authorized_reply()
 	if resp is None:
 		return Response(json.dumps({'message': 'User failed to login'}), status=200, mimetype='application/json')
 	else:
@@ -103,38 +46,31 @@ def logout():
 
 @app.route('/runs')
 def runs():
-	data = make_twitter_request(
-		'statuses/user_timeline',
-		'GET',
-		screen_name=session['twitter_oauth']['screen_name']).data
+	user_name = session['twitter_oauth']['screen_name']
+	data = tw_make_twitter_request('statuses/user_timeline', 'GET', screen_name=user_name).data
 	return json.dumps([
-		get_run_basic_info(t)
+		tw_get_run_basic_info(t)
 		for t
-		in filter_runs(data)])
+		in tw_filter_runs(data)])
 
 
 @app.route('/friends')
 def friends():
-	data = make_twitter_request(
-		'statuses/home_timeline',
-		'GET',
-		screen_name=session['twitter_oauth']['screen_name']).data
+	user_name = session['twitter_oauth']['screen_name']
+	data = tw_make_twitter_request('statuses/home_timeline', 'GET', screen_name=user_name).data
 	return json.dumps([
-		get_run_basic_info(t)
+		tw_get_run_basic_info(t)
 		for t
-		in filter_friends(filter_runs(data), session['twitter_oauth']['user_id'])])
+		in tw_filter_friends(tw_filter_runs(data), session['twitter_oauth']['user_id'])])
 
 
 @app.route('/runs/<tweet_id>')
 def run(tweet_id):
-	data = make_twitter_request(
-		'statuses/show',
-		'GET',
-		id=tweet_id).data
+	data = tw_make_twitter_request('statuses/show', 'GET', id=tweet_id).data
 	return json.dumps([
-		get_run_basic_info(t)
+		tw_get_run_complete_info(t)
 		for t
-		in filter_runs([data])])
+		in tw_filter_runs([data])])
 
 
 if __name__ == '__main__':
