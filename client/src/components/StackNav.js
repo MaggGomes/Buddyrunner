fc1409bc8ea365b93fd981bbb75088c6a98011bc
unknown <magg.slb@gmail.com>
2018-04-22 13:57:04 +0100

    integration of run screen

--- client/src/components/StackNav.js
+++ client/src/components/StackNav.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { StackNavigator } from 'react-navigation';
+import TabNav from './TabNav';
+import RunScreen from './screens/RunScreen';
 import SettingsScreen from './screens/SettingsScreen';
 import AboutScreen from './screens/AboutScreen';
-import TabNav from './TabNav';
 
 const StackNav = StackNavigator (
     {
         Home: {
             screen: TabNav,
+        },
+        GoRun: {
+            screen: RunScreen,
         },
         Settings: {
             screen: SettingsScreen
