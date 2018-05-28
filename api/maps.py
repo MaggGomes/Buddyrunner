14f0d00ed00c54d6a8d9c47621473a5025ae5e08
Bruno Santos <bruno.santos1996@gmail.com>
2018-05-28 05:46:13 +0100

    reverse geocode fix

--- api/maps.py
+++ api/maps.py
@@ -11,7 +11,7 @@ def geo_locate(location):
 
 def maps_get_location(coordinates):
     gmaps = googlemaps.Client(key=google_maps_key)
-    geocode_result = gmaps.reverse_geocode((coordinates['latitude'], coordinates['longitude']))
+    geocode_result = gmaps.reverse_geocode((int(coordinates['latitude']), int(coordinates['longitude'])))
     return geocode_result
     
 
